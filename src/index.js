e34a2d7eac1324d926d473cda0706e947106ae62
Andrew Birt <abirt1986@gmail.com>
2021-12-22 09:47:56 -0500

    move roadtrip into components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { RoadTrip } from './RoadTrip';
+import { RoadTrip } from './components/RoadTrip';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
