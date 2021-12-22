c2a94a3c933160d329b8d942dcedb65ad9957b03
Andrew Birt <abirt1986@gmail.com>
2021-12-22 16:06:14 -0500

    finished layout of components

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,23 +1,57 @@
-import { Form, Row, Col, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Form, Row, Col, Card, Alert, Container, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
-export const Login = () => (
-  <Row className='justify-content-center align-items-center' style={{height: window.innerHeight, width: window.innerWidth}}>
-    <Card className="align-items-center" style={{ padding: '2rem', flexBasis: '420px' }}>
-      <h2>Login</h2>
-      <Card.Body>
-        <Form className="">
-          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <Form.Label column sm="4">
-            Email
-          </Form.Label>
-          <Col sm="8">
-            <Form.Control type="text" />
-          </Col>
-        </Form.Group>
-        </Form>
-      </Card.Body>
-      <p>Don't Have an account? <Link to='/register'>Register</Link></p>
-    </Card>
-  </Row>
-);
+export const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [showDialog, setShowDialog] = useState(false);
+
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    return fetch(`http://localhost:8088/users?email=${email}`)
+    .then(res => res.json()).then(users => users[0]).then(user => {
+      if (user) {
+        localStorage.setItem('roadtrip_user', JSON.stringify(user));
+        navigate('/');
+      } else {
+        setShowDialog(!user);      
+      }
+    });
+  }
+
+  return (
+    <Container>
+      <Alert 
+        variant='danger'
+        className="mt-3"
+        dismissible
+        show={showDialog}
+        onClose={() => setShowDialog(false)}
+      >
+        User doesnt exist
+      </Alert>
+      <Row className='justify-content-center align-items-center' style={{height: window.innerHeight}}>
+        <Card className="align-items-center" style={{ padding: '2rem', flexBasis: '420px' }}>
+          <h2>Login</h2>
+          <Card.Body>
+            <Form className="" onSubmit={handleSubmit}>
+              <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                <Form.Label column sm="3">
+                  Email
+                </Form.Label>
+                <Col sm="9">
+                  <Form.Control onChange={(e) => setEmail(e.target.value)}  type="text" />
+                </Col>
+              </Form.Group>
+              <Button className='form-control' >Login</Button>
+            </Form>
+          </Card.Body>
+          <p>Don't Have an account? <Link to='/register'>Register</Link></p>
+        </Card>
+      </Row>
+    </Container>
+  );
+}
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,23 +1,75 @@
-import { Form, Row, Col, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Form, Row, Col, Card, Container, Alert, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
-export const Register = () => (
-  <Row className='justify-content-center align-items-center' style={{height: window.innerHeight, width: window.innerWidth}}>
-    <Card className="align-items-center" style={{ padding: '2rem', flexBasis: '420px' }}>
-      <h2>Register</h2>
-      <Card.Body>
-        <Form className="">
-          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <Form.Label column sm="4">
-            Email
-          </Form.Label>
-          <Col sm="8">
-            <Form.Control type="text" />
-          </Col>
-        </Form.Group>
-        </Form>
-      </Card.Body>
-      <p>Already Have an account? <Link to='/login'>Login</Link></p>
-    </Card>
-  </Row>
-);
+export const Register = () => {
+  
+  const [user, setUser] = useState({});
+  const [showDialog, setShowDialog] = useState(false);
+  
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    return fetch(`http://localhost:8088/users?email=${user.email}`)
+    .then(res => res.json()).then(users => !!users.length).then(userExists => {
+      if (!userExists) {
+        fetch('http://localhost:8088/users', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(user)
+        }).then(res => res.json()).then(createdUser => {
+          if (createdUser.hasOwnPropery('id')) {
+            localStorage.setItem('roadtrip_user', JSON.stringify(createdUser));
+            navigate('/');
+          }
+        })
+      } else {
+        setShowDialog(userExists);
+      }
+    });
+  }
+
+  return (
+    <Container>
+      <Alert 
+        variant='danger'
+        className="mt-3"
+        dismissible
+        show={showDialog}
+        onClose={() => setShowDialog(false)}
+      >
+        User already exist
+      </Alert>
+      <Row className='justify-content-center align-items-center' style={{height: window.innerHeight}}>
+        <Card className="align-items-center" style={{ padding: '2rem', flexBasis: '420px' }}>
+          <h2>Register</h2>
+          <Card.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                <Form.Label column sm="3">
+                  Email
+                </Form.Label>
+                <Col sm="9">
+                  <Form.Control type="text" onChange={(e) => setUser({ email: e.target.value })} />
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                <Form.Label column sm="3">
+                  Name
+                </Form.Label>
+                <Col sm="9">
+                  <Form.Control type="text" onChange={(e) => setUser({ name: e.target.value })} />
+                </Col>
+              </Form.Group>
+              <Button className='form-control' >Register</Button>
+            </Form>
+          </Card.Body>
+          <p>Already Have an account? <Link to='/login'>Login</Link></p>
+        </Card>
+      </Row>
+    </Container>
+  );
+}
