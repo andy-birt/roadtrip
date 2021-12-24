a96db3a8fa6e43847a3600d563c3802b3ce5c2cf
Andrew Birt <abirt1986@gmail.com>
2021-12-23 22:47:07 -0500

    testing out the map

--- src/components/trip/TripPlan.js
+++ src/components/trip/TripPlan.js
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
+import { Row, Col, Card, Container } from "react-bootstrap";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from 'leaflet';
+import { SearchBox } from "../searchbox/SearchBox";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -11,11 +13,33 @@ L.Icon.Default.mergeOptions({
 });
 
 
+
+
 export const TripPlan = () => {
   const { tripId } = useParams();
+  console.log(L)
   return (
-    
-      <MapContainer style={{height: '100vh'}}  center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <div className="d-flex">
+      <div className="point-of-interest-list">
+        <Container>
+          <Row xs={1} className="g-4 pt-3">
+            {Array.from({ length: 4 }).map((_, idx) => (
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                      This is a longer card with supporting text below as a natural
+                      lead-in to additional content. This content is a little bit longer.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+      <MapContainer style={{height: '100vh', width: '75vw'}}  center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -26,6 +50,6 @@ export const TripPlan = () => {
           </Popup>
         </Marker>
       </MapContainer>
-    
+    </div>
   );
 }
