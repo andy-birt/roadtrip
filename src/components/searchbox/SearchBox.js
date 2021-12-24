05fb4d2f41d2294714c68023013124b0fcdcd98d
Andrew Birt <abirt1986@gmail.com>
2021-12-24 10:00:12 -0500

    add datalist to searchbox container

--- src/components/searchbox/SearchBox.js
+++ src/components/searchbox/SearchBox.js
@@ -12,9 +12,24 @@ Control.SearchBox = Control.extend({
     //* Create the component from JS scratch basically
     //* We need to use DomUtil.create() here to create the element
     //* or else this will throw errors in the application
+
+    const searchBoxContainer = DomUtil.create('div', 'search-container');
+
     const searchInput = DomUtil.create('input', 'form-control');
+    searchInput.setAttribute('list', 'search-results');
     searchInput.placeholder = 'Search Here...';
-    return searchInput;
+    searchBoxContainer.appendChild(searchInput);
+
+    const searchDataList = DomUtil.create('datalist', 'search-results-datalist');
+    searchDataList.id="search-results";
+    searchBoxContainer.appendChild(searchDataList);
+
+    const testResult1 = DomUtil.create('option');
+    testResult1.innerText = 'result1';
+
+    searchDataList.appendChild(testResult1)
+
+    return searchBoxContainer;
   }
 });
 
