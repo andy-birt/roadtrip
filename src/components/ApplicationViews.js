b21678dfc405e0d60781042356d9209f36f82f1f
Andrew Birt <abirt1986@gmail.com>
2021-12-29 11:50:48 -0500

    add search provider to views

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,14 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import { TripList } from "./trip/TripList";
 import { TripProvider } from "./trip/TripProvider";
 import { TripPlan } from "./trip/TripPlan";
+import { SearchProvider } from "./searchbox/SearchProvider";
 
 export const ApplicationViews = ({ user }) => {
   return (
     <TripProvider userId={user.id} >
-      <Routes>
-        <Route path='/' element={<TripList />} />
-        <Route path='/trips/:tripId' element={<TripPlan />} />
-      </Routes>
+      <SearchProvider>
+        <Routes>
+          <Route path='/' element={<TripList />} />
+          <Route path='/trips/:tripId' element={<TripPlan />} />
+        </Routes>
+      </SearchProvider>
     </TripProvider>
   );
 }
