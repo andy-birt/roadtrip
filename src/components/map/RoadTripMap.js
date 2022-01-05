012cfcd46f5ff4fd85ca104c12cae1ebb772030f
Andrew Birt <abirt1986@gmail.com>
2022-01-04 19:15:10 -0500

    create function to alter state, conditionally render selected locations based on if it's routable

--- src/components/map/RoadTripMap.js
+++ src/components/map/RoadTripMap.js
@@ -14,65 +14,82 @@ export const RoadTripMap = ({ homeCoords, pointOfInterests, tripId }) => {
 
   const { routes } = useContext(POIRoutesContext);
 
+  const removeSelectedLocation = (loc) => {
+    const remainingLocations = selectedLocations.filter(sl => sl.textContents !== loc.textContents);
+    setSelectedLocations(remainingLocations);
+  }
+
 
   return (
-    <MapContainer style={{height: '100vh', width: '75vw'}}  center={homeCoords} zoom={13} zoomControl={false} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <SearchBox />
-      <ZoomControl position="bottomleft"/>
-      <Marker position={homeCoords}>
-        <Popup>
-          This is set as your home address <br /> Is this not your home address? Are you lost?
-        </Popup>
-      </Marker>
-      {
-        //* Iterates over the trip locations
-        pointOfInterests.map(poi => (
-          <Marker key={poi.textContents}  position={poi.latlon}>
-            <Popup>
-              {poi.textContents}
-              <div></div>
-              <Button
-                onClick={() => removePointOfInterest(poi.id)}
-              >Remove from Trip</Button>
-            </Popup>
-          </Marker>
-        ))
-      }
-      {
-        //* Iterates over selected locations from the search bar 
-        selectedLocations.map(l => (
-          <Marker key={l.textContents}  position={l.latlon}>
-            <Popup>
-              {l.textContents}
-              <div></div>
-              <Button 
-                onClick={
-                  (e) => savePointOfInterest({
-                    tripId: +tripId,
-                    textContents: l.textContents,
-                    latlon: l.latlon
-                  }).then(() => {
-                    e.target.remove();
-                    document.querySelector('.search-container input').value = '';
-                  })
-                }
-              >Add to Trip</Button>
-              {' '}
-              <Button onClick={() => setSelectedLocations(selectedLocations.filter(sl => sl.textContents !== l.textContents))}>Remove from Map</Button>
-            </Popup>
-          </Marker>
-        ))
-      }
-      {
-        routes.map((route, i) => {
-          const coords = route.map(c => c.reverse());
-          return <Polyline key={i}  pathOptions={{ color: '#0d74d4' }} positions={coords} />
-        })
-      }
-    </MapContainer>
+    <div>
+      <MapContainer style={{height: '100vh', width: '75vw'}}  center={homeCoords} zoom={13} zoomControl={false} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <SearchBox homeCoords={homeCoords} />
+        <ZoomControl position="bottomleft"/>
+        <Marker position={homeCoords}>
+          <Popup>
+            This is set as your home address <br /> Is this not your home address? Are you lost?
+          </Popup>
+        </Marker>
+        {
+          //* Iterates over the trip locations
+          pointOfInterests.map(poi => (
+            <Marker key={poi.id}  position={poi.latlon}>
+              <Popup>
+                {poi.textContents}
+                <div></div>
+                <Button
+                  onClick={() => removePointOfInterest(poi.id)}
+                >Remove from Trip</Button>
+              </Popup>
+            </Marker>
+          ))
+        }
+        {
+          //* Iterates over selected locations from the search bar 
+          selectedLocations.map((l, i) => l.isRoutable ? (
+            <Marker key={i}  position={l.latlon}>
+              <Popup>
+                {l.textContents}
+                <div></div>
+                <Button 
+                  onClick={
+                    () => savePointOfInterest({
+                      tripId: +tripId,
+                      textContents: l.textContents,
+                      latlon: l.latlon
+                    }).then(() => {
+                      removeSelectedLocation(l);
+                      document.querySelector('.search-container input').value = '';
+                    })
+                  }
+                >Add to Trip</Button>
+                {' '}
+                <Button onClick={() => removeSelectedLocation(l)}>Remove from Map</Button>
+              </Popup>
+            </Marker>
+          ) : 
+            <Marker key={i}  position={l.latlon}>
+              <Popup>
+                <p>You cannot make this location part of your current road trip.</p>
+                <p>You can start a new trip within this location.</p>
+                <p>Or maybe you're just a rebel with a hoverboard!</p>
+                <div></div>
+                <Button onClick={() => removeSelectedLocation(l)}>Remove from Map</Button>
+              </Popup>
+            </Marker>
+          )
+        }
+        {
+          routes.map((route, i) => {
+            const coords = route.map(c => c.reverse());
+            return <Polyline key={i}  pathOptions={{ color: '#0d74d4' }} positions={coords} />
+          })
+        }
+      </MapContainer>
+    </div>
   );
 }
