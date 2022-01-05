1e082cd5a3523a7e47d4bf68231400a694cbb1a6
Andrew Birt <abirt1986@gmail.com>
2022-01-05 09:22:21 -0500

    use map context to manipulate the map externally

--- src/components/pointOfInterest/PointOfInterest.js
+++ src/components/pointOfInterest/PointOfInterest.js
@@ -1,16 +1,28 @@
-import { Col, Card } from "react-bootstrap";
 
-export const PointOfInterest = ({ poi }) => (
-  <Col>
-    <Card>
-      <Card.Body>
-        {
-          poi.name ? <Card.Title>{ poi.name }</Card.Title> : null
-        }
-        <Card.Text>
-          { poi.textContents }
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  </Col>
-);
+import { useContext } from "react";
+import { Col, Card } from "react-bootstrap";
+import { RoadTripMapContext } from "../map/RoadTripMapProvider";
+import './PointOfInterest.css';
+
+export const PointOfInterest = ({ poi }) => {
+
+  const { map } = useContext(RoadTripMapContext);
+  
+  return (
+    <Col>
+      <Card 
+        className="point-of-interest"
+        onClick={() => map.panTo(poi.latlon)}  
+      >
+        <Card.Body>
+          {
+            poi.name ? <Card.Title>{ poi.name }</Card.Title> : null
+          }
+          <Card.Text>
+            { poi.textContents }
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
