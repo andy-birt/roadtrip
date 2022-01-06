1c92b676989f92a9e997f83a3964c1b05b4ad85c
Andrew Birt <abirt1986@gmail.com>
2022-01-06 09:32:21 -0500

    use a signal param, set results in then and return results or return error message in the catch if user aborts request

--- src/components/searchbox/SearchProvider.js
+++ src/components/searchbox/SearchProvider.js
@@ -8,10 +8,16 @@ export const SearchProvider = ({ children }) => {
 
   const [ selectedLocations, setSelectedLocations ] = useState([]);
 
-  const getResults = (q) => {
-    return fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${q}&accept-language=en&countrycodes=us&limit=5`)
+  const getResults = (q, signal) => {
+    return fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${q}&accept-language=en&countrycodes=us&limit=5`, { signal })
     .then(res => res.json())
-    .then(results => setResults(results));
+    .then(results => {
+      setResults(results);
+      return results;
+    })
+    .catch((e) => {
+      if (e.name === 'AbortError') return e.name;
+    });
   }
 
   return (
