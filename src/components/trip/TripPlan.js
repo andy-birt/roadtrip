075945c26cb6cf7d89c5d5533f4c5d85b0e79b13
Andrew Birt <abirt1986@gmail.com>
2022-01-10 13:41:05 -0500

    add droppable, make state for the order of point of interests to visit, utilize useEffect to change order of point of interests after dragging and dropping

--- src/components/trip/TripPlan.js
+++ src/components/trip/TripPlan.js
@@ -1,11 +1,13 @@
 import L from 'leaflet';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Container } from "react-bootstrap";
 import { RoadTripMap } from "../map/RoadTripMap";
 import { PointOfInterestContext } from "../pointOfInterest/PointOfInterestProvider";
 import { PointOfInterest } from "../pointOfInterest/PointOfInterest";
 import { POIRoutesContext } from '../poiRoutes/POIRoutesProvider';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { TripContext } from './TripProvider';
 
 
 //! For some reason we have to manually delete the default icon
@@ -23,26 +25,60 @@ L.Icon.Default.mergeOptions({
 
 export const TripPlan = ({ homeCoords }) => {
 
-  const { pointOfInterests, getPointOfInterests, setPointOfInterests } = useContext(PointOfInterestContext);
+  const { pointOfInterests, getPointOfInterests } = useContext(PointOfInterestContext);
 
   const { getRoutes, setRoutes } = useContext(POIRoutesContext);
 
+  const { getTripById, savePointOfInterestOrder } = useContext(TripContext);
+
   const { tripId } = useParams();
 
-  useEffect(() => {
-    getPointOfInterests(tripId)
+  const [ poiIdOrder, setPoiIdOrder ] = useState([]);
+
+  const isArrayEqual = (a, b) => {
+    return JSON.stringify(a) === JSON.stringify(b);
+  }
+
+  const createRoute = (poiIds, tripId) => {
+    getPointOfInterests(tripId, poiIds)
     .then((pois) => {
       if (pois.length !== 0) {
-        setPointOfInterests(pois);
         const POICoords = [ [homeCoords[0], homeCoords[1]], ...pois.map(poi => [poi.latlon.lat, poi.latlon.lng]) ];
         getRoutes(POICoords)
         .then((res) => setRoutes(res));
       } else {
-        setPointOfInterests([]);
         setRoutes([]);
       }
     });
-  }, [tripId, pointOfInterests.length]);
+  }
+
+  const onDragEnd = (res) => {
+
+    const { destination, source, draggableId } = res;
+
+    if (!destination) return;
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+    const newPoiOrder = Array.from(poiIdOrder);
+    newPoiOrder.splice(source.index, 1);
+    newPoiOrder.splice(destination.index, 0, draggableId);
+    setPoiIdOrder(newPoiOrder);
+    savePointOfInterestOrder(tripId, { poiIds: newPoiOrder })
+    .then(() => createRoute(newPoiOrder, tripId));
+  }
+
+  useEffect(() => {
+
+    getTripById(tripId)
+    .then((trip) => {
+      if (!isArrayEqual(trip.poiIds, poiIdOrder)) {
+        setPoiIdOrder(trip.poiIds);
+      }
+      createRoute(trip.poiIds, trip.id);
+    });
+
+  }, [tripId, pointOfInterests.length, poiIdOrder]);
 
   return (
     <div className="d-flex">
@@ -51,11 +87,29 @@ export const TripPlan = ({ homeCoords }) => {
           <Link to="/" >&larr; Trips</Link>
         </Container>
         <Container>
-          <Row xs={1} className="g-4 pt-3">
-            {pointOfInterests.map((poi) => (
-              <PointOfInterest key={poi.id} poi={poi} />
-            ))}
-          </Row>
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId={tripId}>
+              {provided => (
+                <Row 
+                  xs={1} 
+                  className="g-4 pt-3"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {
+                    poiIdOrder.map((poiId, index) => {
+                      const pointOfInterest = pointOfInterests.find(poi => poi.id === +poiId);
+                      if (pointOfInterest) {
+                        return <PointOfInterest key={poiId} poi={pointOfInterest} index={index}/>;
+                      }
+                      return <div key={poiId}/>
+                    })
+                  }
+                  {provided.placeholder}
+                </Row>
+              )}
+            </Droppable>
+          </DragDropContext>
         </Container>
       </div>
       <RoadTripMap homeCoords={homeCoords} pointOfInterests={pointOfInterests} tripId={tripId}/>
