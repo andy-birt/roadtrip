79a2f51d264eff2c7d76d01cea23de105489efb2
Andrew Birt <abirt1986@gmail.com>
2022-01-10 13:36:05 -0500

    add draggable component

--- src/components/pointOfInterest/PointOfInterest.js
+++ src/components/pointOfInterest/PointOfInterest.js
@@ -1,10 +1,11 @@
 
 import { useContext } from "react";
 import { Col, Card } from "react-bootstrap";
+import { Draggable } from "react-beautiful-dnd";
 import { RoadTripMapContext } from "../map/RoadTripMapProvider";
 import './PointOfInterest.css';
 
-export const PointOfInterest = ({ poi }) => {
+export const PointOfInterest = ({ poi, index }) => {
 
   const { map, setLatlng } = useContext(RoadTripMapContext);
 
@@ -14,20 +15,28 @@ export const PointOfInterest = ({ poi }) => {
   }
   
   return (
-    <Col>
-      <Card 
-        className="point-of-interest"
-        onClick={handleClick}  
-      >
-        <Card.Body>
-          {
-            poi.name ? <Card.Title>{ poi.name }</Card.Title> : null
-          }
-          <Card.Text>
-            { poi.textContents }
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
+    <Draggable draggableId={`${poi.id}`} index={index}>
+      {(provided) => (
+        <Col
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <Card 
+            className="point-of-interest"
+            onClick={handleClick}  
+          >
+            <Card.Body>
+              {
+                poi.name ? <Card.Title>{ poi.name }</Card.Title> : null
+              }
+              <Card.Text>
+                { poi.textContents }
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      )}
+    </Draggable>
   );
 }
