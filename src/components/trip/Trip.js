f8a1e366e1c2cfba811e59879eeb9290096400fc
Andrew Birt <abirt1986@gmail.com>
2022-01-10 20:52:32 -0500

    can edit trip details such as name and description now

--- src/components/trip/Trip.js
+++ src/components/trip/Trip.js
@@ -1,10 +1,19 @@
-import { Button, Card } from "react-bootstrap";
+import { useState, useContext } from "react";
+import { Button, Card, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { TripContext } from "./TripProvider";
 import "./Trip.css"
 
-export const Trip = ({ trip }) => {
+export const Trip = ({ trip, setShowModal, setTrip }) => {
+  
+  const { deleteTrip } = useContext(TripContext);
+
+  const [ showDeleteModal, setShowDeleteModal ] = useState(false);
+
   return (
-    <Card className='trip'>
+    <>
+    
+      <Card className='trip'>
         <Card.Body>
           <Card.Title>
             <Link to={`/trips/${trip.id}`}>
@@ -14,16 +23,54 @@ export const Trip = ({ trip }) => {
           <Card.Text>
             {trip.description}
           </Card.Text>
-          <Button variant='outline-success'>
+          <Button 
+            onClick={() => {
+              setShowModal(true);
+              setTrip({
+                ...trip,
+                id: trip.id
+              })
+            }} 
+            variant='outline-success'
+          >
             <i className='bi bi-pencil-square' ></i>
             Edit
           </Button>
           {' '}
-          <Button variant='outline-danger'>
+          <Button 
+            onClick={() => {
+              setShowDeleteModal(true);
+            }}
+            variant='outline-danger'
+          >
             <i className='bi bi-trash'></i>
             Delete
           </Button>
         </Card.Body>
       </Card>
+      <Modal
+        show={showDeleteModal}
+        onHide={() => setShowDeleteModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Delete This Trip?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Are you sure you want to delete "{trip.name}"?</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={() => setShowDeleteModal(false)}
+            variant="success"
+            className="form-control"
+          >Ugh, are we there yet?</Button>
+          <Button 
+            onClick={() => deleteTrip(trip.id)} 
+            variant="danger"
+            className="form-control"
+          >That's it! I'm turning this car around!</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
