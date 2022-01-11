c58d7cfd4973ec8f136b409d42ea11a08cee1890
Andrew Birt <abirt1986@gmail.com>
2022-01-11 11:48:24 -0500

    move input handler into it's own function, made it usable with creating new trips

--- src/components/searchbox/SearchBox.js
+++ src/components/searchbox/SearchBox.js
@@ -6,8 +6,7 @@ import { POIRoutesContext } from "../poiRoutes/POIRoutesProvider";
 import { Spinner } from "react-bootstrap";
 import "./SearchBox.css";
 
-//* This is the actual component we will use in the map
-export const SearchBox = ({ homeCoords }) => {
+export const SearchBox = ({ homeCoords, setStartingLocation }) => {
 
   const map = useMap();
 
@@ -18,6 +17,46 @@ export const SearchBox = ({ homeCoords }) => {
   const [ query, setQuery ] = useState('');
 
   const [ loading, setLoading ] = useState(false);
+
+  const handleInput = (e) => {
+    if (e.nativeEvent.inputType === 'insertText' && e.data !== ' ') {
+      
+      //* Here we are basically setting the query for the search, that's it
+      setQuery(e.target.value);
+
+    } else if (e.nativeEvent.inputType !== 'deleteContentBackward' && e.nativeEvent.inputType !== 'deleteContentForward') {
+      
+      //* When a user clicks or selects one of the results
+      //* Get that result and place it on the map
+      const selected = results.find(r => r.display_name === e.target.value);
+      const location = new LatLng(+selected.lat, +selected.lon);
+      
+
+      //* This condition mainly runs in the trip detail since we have a starting location 
+      //* Else the trip is a new trip and it needs a starting location
+      if (homeCoords) {
+        map.setView(location, 13);
+        getRoutes([[homeCoords[0], homeCoords[1]], [location.lat, location.lng]])
+        .then((res) => {
+          const isRoutable = res ? true : false;
+          setSelectedLocations([
+            ...selectedLocations,
+            {
+              textContents: selected.display_name,
+              latlon: location,
+              isRoutable: isRoutable
+            }
+          ]);
+        });
+      } else {
+        map.setView([location.lat + 0.01, location.lng], 13);
+        setStartingLocation({
+          position: location,
+          textContents: selected.display_name
+        });
+      }
+    }
+  }
 
   useEffect(() => {
     const controller = new AbortController();
@@ -38,29 +77,7 @@ export const SearchBox = ({ homeCoords }) => {
         className="form-control" 
         list="search-results" 
         placeholder="Search Here..." 
-        onInput={
-          (e) => {
-            if (e.nativeEvent.inputType === 'insertText' && e.data !== ' ') {
-              setQuery(e.target.value);
-            } else if (e.nativeEvent.inputType !== 'deleteContentBackward' && e.nativeEvent.inputType !== 'deleteContentForward') {
-              const selected = results.find(r => r.display_name === e.target.value);
-              const location = new LatLng(+selected.lat, +selected.lon);
-              map.panTo(location);
-              getRoutes([[homeCoords[0], homeCoords[1]], [location.lat, location.lng]])
-              .then((res) => {
-                const isRoutable = res ? true : false;
-                setSelectedLocations([
-                  ...selectedLocations,
-                  {
-                    textContents: selected.display_name,
-                    latlon: location,
-                    isRoutable: isRoutable
-                  }
-                ]);
-              });
-            }
-          }
-        }
+        onInput={handleInput}
       />
       { loading && <Spinner animation="border" role="status" /> }
       <datalist 
